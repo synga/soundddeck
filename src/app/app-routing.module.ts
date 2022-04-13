548e25396036d5e7127c2e755749f4d1002e5574
Gabriel Barreto <gabriel-gdr@hotmail.com>
2022-04-13 20:27:55 -0300

    Commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./pages/landing/landing.module').then((m) => m.LandingModule),
+  },
+  {
+    path: 'config',
+    loadChildren: () =>
+      import('./pages/config/config.module').then((m) => m.ConfigModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./pages/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'soundddeck',
+    appId: '1:887625615272:web:fabed02a6e7a1307f6bf9d',
+    storageBucket: 'soundddeck.appspot.com',
+    apiKey: 'AIzaSyB5uczD_toiKEm1ETgc1kJvYvnMa4SxGZg',
+    authDomain: 'soundddeck.firebaseapp.com',
+    messagingSenderId: '887625615272',
+    measurementId: 'G-8QLWFDKJJJ',
+  },
   production: false
 };
 
